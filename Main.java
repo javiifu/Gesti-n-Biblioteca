cc78563e8e2d921187b74b44687e08cc00a6b036
Javier Beltrán <javibeltra89@gmail.com>
2025-05-15 13:25:18 +0200

    poner las cosas bien en main

--- Main.java
+++ Main.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Miscelanea ms = new Miscelanea();        int opcion;
+        Miscelanea ms = new Miscelanea();        
+        int opcion;
         do {
             System.out.println("Bienvenido al menú. ¿Qué desea hacer?");
             System.out.println("1. Añadir un nuevo libro");
